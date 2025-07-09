c94e52ad73254ddd3b76f8358c6ee1675a3b2487
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-07-09 17:46:37 -0600

    i18nSelectPipe
    Se implementa el pipe de internalización i18nSelectPipe para diferenciar entre sexo masculino y femenino y cambiar la palabra "invitarlo" por "invitarla".

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Fernando',
+  gender: 'male',
+  age: 39,
+  address: 'Tampico, México'
+};
+
+const client2 = {
+  name: 'Alejandra',
+  gender: 'female',
+  age: 30,
+  address: 'Texas, Estados Unidos'
+}
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  //* i18n Select pipe
+
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(){
+    if(this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client2);
+  }
+
+
+
+ }
